8e82185e7fba3a702b25e0cd42d568a92048d095
GaddempallyPoojitha <poojithareddygaddempally@gmail.com>
2025-10-24 18:34:07 +0530

    Implement UserService for user management

---
+++ UserService.java
@@ -0,0 +1,13 @@
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
